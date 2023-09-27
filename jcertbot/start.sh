8fcfa3079d6458662fec6d3d73cac7f73635f059
LordJike <lordjike@gmail.com>
2023-09-27 10:09:53 +0200

    new script for certbot

--- jcertbot/start.sh
+++ jcertbot/start.sh
@@ -2,13 +2,23 @@
 
 # Check if certificate files exist
 if [ ! -f "/etc/letsencrypt/live/jike.moe/fullchain.pem" ]; then
-   # Obtain the certificates using Certbot
-        certbot certonly --webroot -w /var/www/certbot -d jike.moe -m lordjike@gmail.com --agree-tos
-        fi
+   echo "Certificate not found. Trying to obtain one..."
+   certbot certonly --webroot -w /var/www/certbot -d *.jike.moe -m lordjike@gmail.com --agree-tos
+   if [ $? -ne 0 ]; then
+       echo "Error obtaining the certificate."
+       exit 1
+   fi
+fi
 
-        # Start the renewal loop
-        trap exit TERM
-        while :; do 
-            certbot renew
-                sleep 12h & wait $!
-                done
+# Start the renewal loop
+trap exit TERM
+while :; do 
+    echo "Attempting certificate renewal..."
+    certbot renew --no-random-sleep-on-renew
+    if [ $? -ne 0 ]; then
+        echo "Error during certificate renewal."
+    else
+        echo "Renewal check completed."
+    fi
+    sleep 12h & wait $!
+done
