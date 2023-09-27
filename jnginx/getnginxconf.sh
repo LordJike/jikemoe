e945885bc7b15353e236b1671fe87e9bfbeaa72c
LordJike <lordjike@gmail.com>
2023-09-27 10:37:21 +0200

    adjustments

--- jnginx/getnginxconf.sh
+++ jnginx/getnginxconf.sh
@@ -2,9 +2,9 @@
 
 # Check environment variable to decide which config to use
 if [ "$ENABLE_SSL" = "true" ]; then
-    cp /etc/nginx/nginx.sslon /etc/nginx/nginx.conf
+    cp /etc/nginx/preconfs/nginx.sslon /etc/nginx/nginx.conf
 else
-    cp /etc/nginx/nginx.ssloff /etc/nginx/nginx.conf
+    cp /etc/nginx/preconfs/nginx.ssloff /etc/nginx/nginx.conf
 fi
 
 # Start Nginx
